c85fa1cbd5b981d7f6c4b1e63165d89e11fae4c0
manhdung <dungli1221@gmail.com>
2023-09-09 17:54:10 +0700

    init3

--- lib/resources/views/frontend/master.blade.php
+++ lib/resources/views/frontend/master.blade.php
@@ -88,7 +88,7 @@
 						<div class="banner-l-item">
 							<a href="#"><img src="img/home/banner-l-4.png" alt="" class="img-thumbnail"></a>
 						</div>
-						<div class="banner-l-item">
+						{{-- <div class="banner-l-item">
 							<a href="#"><img src="img/home/banner-l-5.png" alt="" class="img-thumbnail"></a>
 						</div>
 						<div class="banner-l-item">
@@ -96,7 +96,7 @@
 						</div>
 						<div class="banner-l-item">
 							<a href="#"><img src="img/home/banner-l-7.png" alt="" class="img-thumbnail"></a>
-						</div>
+						</div> --}}
 					</div>
 				</div>
 
